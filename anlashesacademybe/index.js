99f305d7b705fe1f630327c980624d0e18d47072
Tuan Anh <62019384+tanh911@users.noreply.github.com>
2025-11-20 21:42:17 +0100

    Update index.js

--- anlashesacademybe/index.js
+++ anlashesacademybe/index.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
 const PORT = process.env.PORT || 5000;
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173","https://anlashweb.onrender.com"],
+    origin: ["http://localhost:3000", "http://localhost:5173","https://anlashweb.vercel.app"],
     credentials: true,
   })
 );
