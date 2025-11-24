03cc5a82477e19604988b0bd38322c386a441284
Tuan Anh <62019384+tanh911@users.noreply.github.com>
2025-11-24 14:01:25 +0100

    Change API base URL to production server

--- anlashesacademy/src/pages/Login.jsx
+++ anlashesacademy/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import "./Login.css";
 
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = "https://anlashwebbe.onrender.com/api";
 
 const Login = ({ setAdmin }) => {
   const [username, setUsername] = useState("");
